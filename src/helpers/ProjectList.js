042ce902ac90a6e8f81b2415b29226b273c203a1
sarath2370 <70150388+sarath2370@users.noreply.github.com>
2023-01-10 02:56:10 -0500

    Update ProjectList.js

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -7,11 +7,13 @@ export const ProjectList = [
     {
         name: "Angle Management ",
         image: Proj1,
-        skills: "ELM , Design Thinking"
+        skills: "ELM , Design Thinking",
+        link: "https://github.com/sarath2370"
     },
     {
         name: "Asl Detector",
         image: Proj2,
-        skills: "Python, OpenCV, CVZone, Google trainable machine"
+        skills: "Python, OpenCV, CVZone, Google trainable machine",
+        link: "https://github.com/sarath2370/ASL_Detect"
     },
 ];
